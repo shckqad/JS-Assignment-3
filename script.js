bae74ded1a1f95ebac9c17741eb55674e88c7a33
shckqad <87878268+shckqad@users.noreply.github.com>
2021-11-29 22:55:00 -0500

    Wap
    Syntax corrections

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Creating an Object Model for ID card and coming up with a formula to work expiry date //
-function ID(name, expiryDate) {
+function idCard(name, expiryDate) {
   this.name = name;
   this.expiryDate = new Date(expiryDate);
   this.currentDay = new Date();
@@ -11,34 +11,33 @@ function ID(name, expiryDate) {
      } else{
        return false;
      }
-   }
+   };
 this.expiry = this.checkExpiration();
-}
 //ID card array
-var visitorid1 = new idCard('Jack Black', '2022/06/03');
-var visitorid2 = new idCard('Bellela Thorne', '2022/06/03');
-var visitorid3 = new idCard('Amanda Joseph', '2020/06/03');
+var visitor1 = new idCard('Jack Black', '2022/06/03');
+var visitor2 = new idCard('Bellela Thorne', '2022/06/03');
+var visitor3 = new idCard('Amanda Joseph', '2020/06/03');
 
 var idArray = [visitor1, visitor2, visitor3];
 //vaccine card object model
-function vCard(name, doseTwo, doseOne){
+function vaxCard(name, doseTwo, doseOne){
  this.name = name;
  this.doseOne = new Date (doseOne);
  this.doseTwo = new Date(doseTwo);
- his.checkExpiry = function(index) {
+ this.checkExpiry = function(index) {
     if (idArray[index].name == this.name){
         return true;
     } else{
         return false;
     }
-  }
+  };
 
 this.checkDays = function(){
   this.difference = this.today.getTime() - this.doseTwo.getTime();
-  difference = Math.floor(difference / 86400000);
+  var difference = Math.floor(difference / 86400000);
   return difference;
-}
-    this.checkStatus = function(index){
+};
+    var checkStatus = function(index){
       if(idArray[index].expiry == false) {
         return 'ID expired';
       }
@@ -57,21 +56,20 @@ this.checkDays = function(){
       else{
         return 'Fully Vaccinated!';
       }
-   }
+   };
 }
 //creating vaccine array.
 
-var visitor1 = new vaxCard('Jack Black', '2022/11/03');
-var visitor2 = new vaxCard('Bellela Thorne', '2022/12/03');
-var visitor3 = new vaxCard('Amanda Joseph', '2020/06/03');
+var visitor4 = new vaxCard('Jack Black', '2022/11/03');
+var visitor5 = new vaxCard('Bellela Thorne', '2022/12/03');
+var visitor6 = new vaxCard('Amanda Joseph', '2020/06/03');
 
-var vaxCardArray = [visitor1, visitor2, visitor3]
+var vaxCardArray = [visitor4, visitor5, visitor6];
 
-var vaxCardArrayLength = vaxCardArray.length;
 for (var i = 0; i < vaxCardArray.length; i++) {
-document.write("<tr>")
-document.write("<td>", idArray[i].name, "</td>")
-document.write("<td>", vaxCardArray[i].checkDifference(i), "</td>")
-document.write("</tr>")
+document.write("<tr>");
+document.write("<td>", idArray[i].name, "</td>");
+document.write("<td>", vaxCardArray[i].checkDifference(i), "</td>");
+document.write("</tr>");
 }
 }
