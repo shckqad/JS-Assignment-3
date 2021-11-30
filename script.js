75bf18da87ccd9ed079388e269058dfef89e2588
shckqad <87878268+shckqad@users.noreply.github.com>
2021-11-29 21:46:17 -0500

    Wanna Die

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ var visitorid3 = new idCard('Amanda Joseph', '2020/06/03');
 
 var idArray = [visitor1, visitor2, visitor3];
 //vaccine card object model
-function vCard(name, doseTwo){
+function vCard(name, doseTwo, doseOne){
  this.name = name;
- this.vaccinated = True;
+ this.doseOne = new Date (doseOne);
  this.doseTwo = new Date(doseTwo);
  his.checkExpiry = function(index) {
     if (idArray[index].name == this.name){
@@ -31,7 +31,33 @@ function vCard(name, doseTwo){
     } else{
         return false;
     }
+  }
+
+this.checkDays = function(){
+  this.difference = this.today.getTime() - this.doseTwo.getTime();
+  difference = Math.floor(difference / 86400000);
+  return difference;
 }
+    this.checkStatus = function(index){
+      if(idArray[index].expiry == false) {
+        return 'ID expired';
+      }
+      else if(idArray[index].name == this.name){
+        return 'Fraud! Name does not match';
+      }
+      else if (!this.doseOne){
+        return 'NO VACCINATION';
+      }
+      else if(!this.doseTwo){
+        return 'NO SECOND DOSE!';
+      }
+      else if(this.checkDays() < 14){
+        return '14 DAYS HAVE NOT PASSED!';
+      }
+      else{
+        return 'Fully Vaccinated!';
+      }
+   }
 }
 //creating vaccine array.
 
@@ -41,6 +67,11 @@ var visitor3 = new vaxCard('Amanda Joseph', '2020/06/03');
 
 var vaxCardArray = [visitor1, visitor2, visitor3]
 
-for (var a = 0; a <idArray.length; a++){
-  console.log(idArray[a].name)
+var vaxCardArrayLength = vaxCardArray.length;
+for (var i = 0; i < vaxCardArray.length; i++) {
+document.write("<tr>")
+document.write("<td>", idArray[i].name, "</td>")
+document.write("<td>", vaxCardArray[i].checkDifference(i), "</td>")
+document.write("</tr>")
+}
 }
